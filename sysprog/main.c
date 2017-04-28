12ba7a02c02148a37e816da9dc57ed41f2d968e8
ryuneeee <ryuneeee@gmail.com>
2017-04-28 17:14:58 +0900

    read line

--- sysprog/main.c
+++ sysprog/main.c
@@ -3,13 +3,19 @@
 int main() {
 
 	FILE* fp;
-	char str[100];
-	fp = fopen("FIGURE2-5(TAB).txt", "r");
-	while (!feof(fp)) {
-		fscanf(fp, "%s", &str);
-		printf("%s\n", str);
-	}
+	char * line = NULL;
+	size_t len = 0;
 
-    return 0; //test
+	fp = fopen("FIGURE2-5(TAB).txt", "r");
+	// while (!feof(fp)) {
+		// fscanf(fp, "%s", &str);
+		// printf("%s\n", str);
+	// }
+	while (getline(&line, &len, fp) != -1) {
+        printf("%s", line);
+    }
+
+
+    return 0;
 }
 
