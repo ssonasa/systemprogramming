9660ff34429584c1c44894013bb563ae5d34910b
손 <@SON>
2017-04-28 16:58:44 +0900

    read sourceCode

--- sysprog/main.c
+++ sysprog/main.c
@@ -2,6 +2,14 @@
 
 int main() {
 
+	FILE* fp;
+	char str[100];
+	fp = fopen("FIGURE2-5(TAB).txt", "r");
+	while (!feof(fp)) {
+		fscanf(fp, "%s", &str);
+		printf("%s\n", str);
+	}
+
     return 0; //test
 }
 
