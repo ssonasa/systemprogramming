ed0b4b3b6f51475558acadd39149edac6ee5107c
ryuneeee <ryuneeee@gmail.com>
2017-04-29 00:57:45 +0900

    파일 읽어오기, 리팩토링

--- sysprog/main.c
+++ sysprog/main.c
@@ -1,54 +1,53 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#define LINE_BUF_SIZE 1024
 
 void file_open();
-
+void printAsmMap();
 char *asmMap[51][4] = {};
 
 int main() {
-	file_open();
+	file_open("FIGURE2-5(TAB).txt");
+	printAsmMap();
     return 0;
 }
 
+int isBlankLabel(char* str){
+	return isspace(str[0]);
+}
 
-void file_open(){
-	FILE* fp;
-	size_t len = 0;
-	int linenum = 0;
+int isCommentLine(char* str){
+	return str[0] == '.';
+}
 
-	fp = fopen("FIGURE2-5(TAB).txt", "r");
-	int buf_size = 1024;
-	char line[buf_size];
-	while(fgets(line, buf_size, fp)) {
+void file_open(char* filename){
+	char line[1024];
+	FILE* fp = fopen(filename, "r");
 
-		char *ch;
+	for(int linenum=0; fgets(line, LINE_BUF_SIZE, fp); linenum++){
 		int i=0;
+		if (isBlankLabel(line)) asmMap[linenum][i++] = "\t";
+		if (isCommentLine(line)) continue;
 
-		if (isspace(line[0])){
-			asmMap[linenum][0] = NULL;
-			i++;
-		}
-		
 
-		ch = strtok(line, " 	\n");
-		while (ch != NULL) {
-			i++;
-			asmMap[linenum][i] = ch;
-			// printf("%s", asmMap[linenum][i]);
-			// printf("%s/", ch);
+		char* ch = strtok(line, " 	\n");
+		for(; ch != NULL; i++){
+			asmMap[linenum][i] = (char*) malloc(strlen(ch));
+			strcpy(asmMap[linenum][i], ch);
 			ch = strtok(NULL, " 	\n");
 		}
-
-		// printf("%d\n", i);
-		// printf("%d", linenum);
-		linenum++;
 	}
+	fclose(fp);
+}
 
-	for(int i=0; i<10; i++){
+void printAsmMap(){
+	for(int i=0; i<51; i++){
 		for(int j=0; j<4; j++){
-			printf("%s	", asmMap[i][j]);
+			if(asmMap[i][j]!=NULL)
+				printf("%s/", asmMap[i][j]);
 		}
 		printf("\n");
 	}
+
 }
