034c5b1e56b26956ef72c1e499a0edfe7de2a118
ryuneeee <ryuneeee@gmail.com>
2017-04-30 17:09:16 +0900

    pass1 complete!

--- sysprog/pass1.c
+++ sysprog/pass1.c
@@ -19,21 +19,21 @@ char* opcodes[] = {"ADD", "ADDF", "ADDR", "AND", "CLEAR", "COMP", "COMPF", "COMP
                         "LDB", "LDCH", "LDF", "LDL", "LDS", "LDT", "LDX", "LPS", "MUL", "MULF",
                         "MULR", "NORM", "OR", "RD", "RMO", "RSUB", "SHIFTL", "SHIFTR", "SIO", "SSK",
                         "STA", "STB", "STCH", "STF", "STI", "STL", "STS", "STSW", "STT", "STX",
-                        "SUB", "SUBF", "SUBR", "SVC", "TD", "TIO", "TIX", "TIXR", "WD", "+JSUB"};
+                        "SUB", "SUBF", "SUBR", "SVC", "TD", "TIO", "TIX", "TIXR", "WD", "+JSUB", "+LDT"};
 
 char* machinecodes[] = {"18", "58", "90", "40", "B4", "28", "88", "A0", "24", "64",
                             "9C", "C4", "C0", "F4", "3C", "30", "34", "38", "48", "00",
                             "68", "50", "70", "08", "6C", "74", "04", "D0", "20", "60",
                             "98", "C8", "44", "D8", "AC", "4C", "A4", "A8", "F0", "EC",
                             "0C", "78", "54", "80", "D4", "14", "7C", "E8", "84", "10",
-                            "1C", "5C", "94", "B0", "E0", "F8", "2C", "B8", "DC", "48"};
+                            "1C", "5C", "94", "B0", "E0", "F8", "2C", "B8", "DC", "48", "74"};
 
 int size[] = {3, 3, 2, 3, 2, 3, 3, 2, 3, 3,
                 2, 1, 1, 1, 3, 3, 3, 3, 3, 3, 
                 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 
                 2, 1, 3, 3, 2, 3, 2, 2, 1, 3,
                 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 
-                3, 3, 2, 2, 3, 1, 3, 2, 3, 4};
+                3, 3, 2, 2, 3, 1, 3, 2, 3, 4, 4};
 
 int main(){
     // printf("%s\n", machinecodes[60]);
@@ -49,11 +49,8 @@ int isExistLabel(char* label){
 }
 
 char* getMachineCode(char* opcode){
-    // printf("[%s]", opcode);
-	// for(int i=0; opcodes[i]!=NULL; i++){
-	for(int i=0; i<60; i++){
+	for(int i=0; i<sizeof(machinecodes)/sizeof(char*); i++){
 		if(!strcmp(opcodes[i], opcode)){
-            printf("[%d, opcodes: %s]", i, opcode);
 			return machinecodes[i];
 		}else{
         }
@@ -99,7 +96,6 @@ char** parseLine(char* line){
 int addSYMTAB(char* label, int loc){
 	int i;
 	for(i=0; symTab[i] != NULL; i++){}
-	// printf("\nADD SYM[%d]: %s", i, label);
 	symrow* row = (symrow*)malloc(sizeof(symrow)); 
 	row->label = label;
 	row->loc = loc;
@@ -124,8 +120,8 @@ void pass1(char* filename, char* output){
         if(!strcmp(opcode, "END")) break;
 
         if(linenum == 1 && !strcmp(opcode, "START")) { 
-            start_address = loc; // set start_address to locctr
             sscanf(operand,"%x",&loc); // start address.
+            start_address = loc; // set start_address to locctr
             fprintf(fpw, "%04x\t%s\t%s\t%s\n", loc, label, opcode, operand); // write intermediate file.
             continue;
         }
@@ -141,10 +137,12 @@ void pass1(char* filename, char* output){
 		}
 
 		//search optab for opcode
+        fprintf(fpw, "%04x \t %s", loc, line); // write intermediate file.
+
 		char* code = getMachineCode(opcode);
 		if(code != NULL){ //TODO: add 3 {instruction length} to LOCCTR
             loc += getOpcodeSize(opcode);
-            // sscanf(code, "%d", &temp);
+            sscanf(code, "%d", &temp);
             // loc += temp;
 		}else if(!strcmp(opcode, "WORD")){ // TODO: add 3 to LOCCTR
             loc += 3;
@@ -155,23 +153,13 @@ void pass1(char* filename, char* output){
             sscanf(operand, "%d", &temp);
             loc += 3*temp;
 		}else if(!strcmp(opcode, "BYTE")){
+            loc += getBytesLength(operand);
+		}else if(!strcmp(opcode, "BASE")){
+            
+        }else{
+			printf("\nError: Invalid operation code: %s", opcode); break;
 
-            // loc += getBytesLength(operand);
-			//TODO
-			// 1. find length of constant in bytes.
-			// 2. add length to LOCCTR.
-		}else{
-			// printf("%s", opcode);
-			// printf("\nError: Invalid operation code: %s", opcode); break;
-			//TODO: invalid opcode error.
 		}
-
-
-		//TODO
-        printf("%s", line); // write intermediate file.
-        fprintf(fpw, "%04x \t %s", loc, line); // write intermediate file.
-		// write line to intermediate file.
-
 	}
 
 
@@ -179,6 +167,7 @@ void pass1(char* filename, char* output){
 	//TODO
 	// write last line to intermediate file.
 	// save (LOCCTR - starting address) as program length
+    fprintf(fpw, "length: %04x", loc - start_address); // write intermediate file.
 
 
 	//END Pass 1
